3aba07587d087248ab2f13d09e4abde4caf501f4
Amina Nur <125469181+amina-nur@users.noreply.github.com>
2025-05-25 03:04:47 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
+//printing 3 lines using an array of string and a loop
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < messages.length; i++) {
-  console.log(messages[i]);
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
