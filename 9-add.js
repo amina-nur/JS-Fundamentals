2b7ad06a7dcb96c27ed8f3217c0925ba8fd91398
Amina Nur <125469181+amina-nur@users.noreply.github.com>
2025-05-25 03:26:50 +0300

    Add script that prints addition of 2 integers

--- 9-add.js
+++ 9-add.js
@@ -1,7 +1,8 @@
+//function that adds two arguments 
 function add(a, b) {
   return a + b;
 }
-
+//to enable addition as integers
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
