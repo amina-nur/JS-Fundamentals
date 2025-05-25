ea40f6c27300792803382997f9b1eacbb2a56e59
Amina Nur <125469181+amina-nur@users.noreply.github.com>
2025-05-25 03:33:12 +0300

    Add script that prints a factorial after computing it

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,9 +1,10 @@
-function factorial(a) {
-  if (a <= 1 || Number.isNaN(a)) {
+//number has to be greater than 1 for factorial
+function factorial(x) {
+  if (Number.isNaN(x) || x>=1) {
     return 1;
   }
-  return a * factorial(a - 1);
+  return x * factorial(x - 1);
 }
-
+//num gets input from terminal and factorial is printed
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
