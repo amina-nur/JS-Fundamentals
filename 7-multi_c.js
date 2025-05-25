dc8805123b620f990e3fa41c137f8ce0a0158c96
Amina Nur <125469181+amina-nur@users.noreply.github.com>
2025-05-25 03:11:50 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 
 const x = parseInt(process.argv[2]);
 
+//Number.isNan checks if value is exactly pecial "Nan" value(not a number)
 if (Number.isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let j = 0; j < x; j++) {
     console.log('C is fun');
   }
 }
