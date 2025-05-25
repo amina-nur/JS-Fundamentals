616d797d2eef35808dbb5d2c51a03bd5b0847b67
Amina Nur <125469181+amina-nur@users.noreply.github.com>
2025-05-25 03:19:33 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,10 @@
+//take first argument, convert to integer and store in constant size
 const size = parseInt(process.argv[2]);
 
 if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
+  for (let k = 0; k < size; k++) {
     console.log('X'.repeat(size));
   }
 }
